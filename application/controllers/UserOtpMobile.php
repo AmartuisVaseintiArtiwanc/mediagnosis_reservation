ca87116a6ab0764008e274dbbc395737a7d39e6b
Satria W Sandi <amartuis@gmail.com>
2016-10-04 22:06:39 +0700

    kebutuhan otp

---
+++ application/controllers/UserOtpMobile.php
@@ -0,0 +1,86 @@
+<?php  
+	class UserOtpMobile extends CI_Controller{
+		function __construct(){
+			parent::__construct();
+
+			$this->load->helper(array('form', 'url'));
+			$this->load->helper('date');
+			$this->load->helper('html');
+		    $this->load->library("pagination");
+		    $this->load->library('form_validation');
+		    $this->load->library('email');
+
+		    $this->load->model("Patient_model");
+		    $this->load->model("UserOtp_model");
+		}
+
+		function generateUserOtp(){
+			$datetime = date('Y-m-d H:i:s', time());
+			$expireTime = $datetime;//->modify("+30 minutes");
+			$OtpCode = $this->input->post("otpCode"); 
+			$userID = $this->input->post("userID");
+            $patient_data = $this->Patient_model->getPatientByUserID($userID);
+            $patientID = $patient_data->patientID;
+            $exisitingOtp_data = $this->UserOtp_model->checkOtpByPatientID($OtpCode);
+
+            if($userID == null || $userID == ""){
+            	echo json_encode("empty");
+            }
+            else{
+            if($exisitingOtp_data == 1){
+            	//udah ada
+            	$data_otp = array(
+            			'otpCode' => $OtpCode,
+            			'expireTime' => $expireTime,
+            			'lastUpdated' => $datetime,
+            			'lastUpdatedBy' => $userID
+            		);
+
+            	$this->db->trans_begin();
+            	$query = $this->UserOtp_model->updateOtp($patientID ,$data_otp);
+            	if ($this->db->trans_status() === FALSE) {
+	                // Failed to save Data to DB
+	                $this->db->trans_rollback();
+	                $status = 'error';
+					$msg = "Maaf, Terjadi kesalahan saat melakukan generasi otp";
+	            }
+	            else{
+	            	$this->db->trans_commit();
+        			$status = 'success';
+					$msg = "Proses generasi otp berhasil";
+	            }
+
+            }else{
+            	//belom ada
+            	$data_otp = array(
+            			'patientID' => $patientID,
+            			'otpCode' => $OtpCode,
+            			'expireTime' => $expireTime,
+            			'isActive' => 1,
+            			'created' => $datetime,
+            			'createdBy' => $userID,
+            			'lastUpdated' => $datetime,
+            			'lastUpdatedBy' => $userID
+            		);
+
+            	$this->db->trans_begin();
+            	$query = $this->UserOtp_model->createOtp($data_otp);
+            	if ($this->db->trans_status() === FALSE) {
+	                // Failed to save Data to DB
+	                $this->db->trans_rollback();
+	                $status = 'error';
+					$msg = "Maaf, Terjadi kesalahan saat melakukan generasi otp";
+	            }
+	            else{
+	            	$this->db->trans_commit();
+        			$status = 'success';
+					$msg = "Proses generasi otp berhasil";
+	            }
+            }
+		echo json_encode(array('status' => $status, 'msg' => $msg));
+		}
+		
+
+		}
+	}
+?>
---
+++ application/models/UserOtp_model.php
@@ -0,0 +1,31 @@
+<?php 
+	class UserOtp_model extends CI_Model {
+
+
+		function createOtp($data){
+		    $this->db->insert('tbl_cyberits_t_user_otp',$data);
+			$result=$this->db->affected_rows();
+			return $result;
+		}
+
+		function updateOtp($patientID,$data){
+			$this->db->where('patientID',$patientID);
+			$this->db->update('tbl_cyberits_t_user_otp',$data);
+			$result=$this->db->affected_rows();
+			return $result;
+		}
+
+		function checkOtpByPatientID($OtpCode){
+			$this->db->select('*');
+		    $this->db->from('tbl_cyberits_t_user_otp a');
+		    $this->db->where('a.otpCode',$OtpCode);
+		    
+		    $query = $this->db->get();
+	        if($query->num_rows()>0){
+	            return 1; // allready exist
+	        }else{
+	            return 0; //blom ada
+	        }
+		}
+	}
+?>
